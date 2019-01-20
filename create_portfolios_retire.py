c53a45a985be950daeecaac1a899c2af024f3b84
Andrew Steffey <asteffey@gmail.com>
2019-01-20 14:33:05 -0500

    Baseline before refactor #11

--- create_portfolios_retire.py
+++ create_portfolios_retire.py
@@ -33,20 +33,9 @@ def get_returns_over_timeframe(yearly_returns: pd.Series, timeframe):
     return pd.Series(returns, index=start_years_range)
 
 
-def get_random_portfolios(num_portfolios, historic_data: pd.DataFrame, inflation_data, timeframe, portfolio_weights = None):
-    num_funds = len(historic_data.columns)
-
+def get_random_portfolios(historic_data: pd.DataFrame, inflation_data, timeframe, portfolio_weights):
     portfolios_yearly_returns = []
     portfolios_returns_over_timeframe = []
-
-    portfolio_weights = [get_random_weights(
-        num_funds) for n in range(num_portfolios)] if portfolio_weights is None else list(portfolio_weights)
-
-    #ensure portfolios with 100% of each asset is accounted for
-    # for i in range(num_funds):
-    #     weights = [0]*num_funds
-    #     weights[i] = 1
-    #     portfolio_weights.append(np.array(weights))
 
     for weights in progressbar.progressbar(portfolio_weights):
         
@@ -75,26 +64,14 @@ historic_financials = pd.read_csv("historic_financials.csv", index_col=0, squeez
 risk_free_rate = historic_financials['RISK_FREE']
 inflation_data = historic_financials['USA_INF']
 
-historic_data = pd.read_csv("historic_data.csv", index_col=0) / 100
-historic_data = historic_data.drop(labels='USA_BILL',axis=1)
-# historic_data = historic_data.drop(labels='USA_REIT',axis=1)
+historic_data = pd.read_csv("historic_data_retire.csv", index_col=0) / 100
 
+#0          1           2       3           4           5   6   7
+#USA_TSM    WLDx_TSM	USA_INT	USA_BILL	USA_REIT	GLD	EM	SCB
 
-custom_weights = [np.array([0.39, .35, .11, .15]),
-                  np.array([0.29, .28, .28, .15]),
-                  np.array([0.66, .01, .18, .15])]
+custom_weights = range_of_allocations(20,8)
+custom_weights = filter(lambda x : x[0]>=0.15 and x[0]<=0.4 and x[1]<=0.2 and x[2]<=0.2 and x[3]<=0.2 and x[4]<=0.15 and x[5]<=0.2 and x[6]<=0.3 and x[7]<=0.2, custom_weights)
 
-# custom_weights = [np.array([0.46, .23, .16, .15]),
-#                   np.array([1, 0, 0, 0, 0])]
+random_portfolios = get_random_portfolios(historic_data, inflation_data, 20, list(custom_weights))
 
-custom_weights = None
-
-NUM_PORTFOLIOS = 50000
-
-custom_weights = range_of_allocations(100,4)
-custom_weights = filter(lambda x : x[0]>=0.2 and x[1]>=0.1 and x[2]>=0.1 and x[3]<=0.15, custom_weights)
-
-random_portfolios = get_random_portfolios(NUM_PORTFOLIOS, historic_data, inflation_data, 15, custom_weights)
-
-pickle.dump(random_portfolios, open('range_of_portfolios_100_15.bin',mode='wb'))
-#random_portfolios_5E4_16_tbill
+pickle.dump(random_portfolios, open('range_of_portfolios_20_retire20.bin',mode='wb'))
