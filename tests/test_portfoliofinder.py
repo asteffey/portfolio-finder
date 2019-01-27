fd6728b575b9b113b95040ca0cb524c789619e74
Andrew Steffey <asteffey@gmail.com>
2019-01-27 15:14:40 -0500

    Add test_get_specific_returns

--- tests/test_portfoliofinder.py
+++ tests/test_portfoliofinder.py
@@ -10,9 +10,15 @@ def test_fetch_all_returns_from_csv():
     assert returns['USA_TSM'][1973] == pytest.approx(-0.177)
     assert returns['GLD'].sum() == pytest.approx(5.13)
 
+def test_get_specific_returns(returns, symbols):
+    all_returns = pf.fetch_all_returns_from_csv("tests/test_data.csv")
+    returns = pf.get_specific_returns(all_returns, ['USA_TSM', 'GLD', 'EM'])
+    assert all(returns.axes[0].array == list(range(1970,2018)))
+    assert all(returns.axes[1].array == ['USA_TSM', 'GLD', 'EM'])
+    assert returns['USA_TSM'][1970] == pytest.approx(0.009)
+    assert returns['USA_TSM'][1973] == pytest.approx(-0.177)
+    assert returns['GLD'].sum() == pytest.approx(5.13)
 
-#TODO test get_specific_returns(returns, symbols)
-    
 #TODO get_portfolio_returns (portfolio_allocation, fund_returns)
     
 #TODO get_inflation_adjusted_returns (returns, inflation_rates)
