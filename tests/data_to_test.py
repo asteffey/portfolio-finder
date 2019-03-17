368f46ac551cdd7f2dbdec71f9ac662d234bd365
=Andrew Steffey <asteffey@gmail.com>
2019-03-17 00:27:31 -0400

    Clean test_returns_by_symbol
    Migrate logic in dtt to main test module

--- tests/data_to_test.py
+++ tests/data_to_test.py
@@ -39,6 +39,14 @@ CUSTOM_STATISTICS = ['min',stats.percentile_for(10),stats.gmean]
 
 _TEST_DATA_PATH = "tests/test_results.xlsx"
 
+def read_dataframe(sheet_name, index_col=0, usecols=None):
+    return pd.read_excel(_TEST_DATA_PATH, sheet_name, index_col=index_col, usecols=usecols)
+
+def read_series(sheet_name, index_col=0, usecols=None):
+    return pd.read_excel(_TEST_DATA_PATH, sheet_name, index_col=index_col, usecols=usecols, squeeze=True)
+
+def read_dataframe_raw(sheet_name):
+    return pd.read_excel(_TEST_DATA_PATH, sheet_name, header=None)
 
 def get_expected_all_returns() -> pd.DataFrame:
     """returns expected_all_returns read from excel file"""
--- tests/test_returns_by_symbol.py
+++ tests/test_returns_by_symbol.py
@@ -8,8 +8,16 @@ from pandas.util.testing import assert_series_equal
 import pandas as pd
 
 import portfoliofinder as pf
-import data_to_test as dtt
 
+from data_to_test import read_dataframe
+from data_to_test import read_series
+
+SPECIFIC_FUNDS = ['USA_TSM', 'GLD', 'EM']
+
+EXPECTED_ALL_RETURNS = read_dataframe('all_returns')
+EXPECTED_SPECIFIC_RETURNS = read_dataframe('specific_returns')
+EXPECTED_INFLATION_RATES = read_series('inflation_rates')
+EXPECTED_INFLATION_ADJUSTED_SPECIFIC_RETURNS = read_dataframe('inflation_adjusted_specific_returns')
 
 def test_init():
     df = pd.DataFrame({'a':[1],'b':[2]})
@@ -23,24 +31,20 @@ def test_from_csv():
     """tests fetch_all_returns_from_csv"""
     actual_returns = pf.ReturnsBySymbol.from_csv("tests/test_data.csv")
 
-    expected = dtt.get_expected_all_returns()
-    assert_frame_equal(actual_returns.to_dataframe(), expected)
+    assert_frame_equal(actual_returns.to_dataframe(), EXPECTED_ALL_RETURNS)
 
 
 def test_get_specific_returns():
     """tests get_specific_returns"""
-    all_returns = pf.ReturnsBySymbol(dtt.get_expected_all_returns())
+    all_returns = pf.ReturnsBySymbol(EXPECTED_ALL_RETURNS)
 
-    actual_specific_returns = all_returns.filter_by_symbols(dtt.SPECIFIC_FUNDS)
+    actual_specific_returns = all_returns.filter_by_symbols(SPECIFIC_FUNDS)
 
-    expected = dtt.get_expected_specific_returns()
-    assert_frame_equal(actual_specific_returns.to_dataframe(), expected)
+    assert_frame_equal(actual_specific_returns.to_dataframe(), EXPECTED_SPECIFIC_RETURNS)
 
 def test_adjust_for_inflation():
     """tests get_inflation_adjusted_returns"""
-    specific_returns = pf.ReturnsBySymbol(dtt.get_expected_specific_returns())
-    inflation_rates = dtt.get_expected_inflation_rates()
-    actual_inflation_adjusted_returns = specific_returns.adjust_for_inflation(inflation_rates)
+    specific_returns = pf.ReturnsBySymbol(EXPECTED_SPECIFIC_RETURNS)
+    actual_inflation_adjusted_returns = specific_returns.adjust_for_inflation(EXPECTED_INFLATION_RATES)   
 
-    expected = dtt.get_expected_inflation_adjusted_specific_returns()
-    assert_frame_equal(actual_inflation_adjusted_returns.to_dataframe(), expected)
+    assert_frame_equal(actual_inflation_adjusted_returns.to_dataframe(), EXPECTED_INFLATION_ADJUSTED_SPECIFIC_RETURNS)
