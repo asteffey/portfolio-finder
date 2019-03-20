f2f8704ff2efd7e0e97476866f88d5a266853a95
=Andrew Steffey <asteffey@gmail.com>
2019-03-19 21:52:22 -0400

    Create DataByStartYearByAllocation
    Parent class to make these DRYer:
    - PortfolioTimeframesByStartYearByAllocation
    - PortfolioValuesByStartYearByAllocation

---
+++ portfoliofinder/data_by_startyear_by_allocation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from functools import reduce
+
+from .contributions import Contributions
+from ._convert_to_dataframe_by_allocation import _convert_to_dataframe_by_allocation
+from .stats import DEFAULT_STATS, get_statistics_by_allocation
+
+class DataByStartYearByAllocation():
+
+    def __init__(self, data_func, data_by_allocation: dict, *argv):
+        self._data = _get_data_by_startyear_by_allocation(data_func, data_by_allocation, *argv)
+
+    def to_series(self, allocation) -> pd.Series:
+        return self._data[allocation]
+
+    def to_dataframe(self) -> pd.DataFrame:
+        return _convert_to_dataframe_by_allocation(self._data)
+
+    def get_statistics(self, statistics = DEFAULT_STATS) -> pd.Series:
+        return get_statistics_by_allocation(self._data, statistics)
+
+
+def _get_data_by_startyear_by_allocation(data_func, data_by_allocations, *argv):
+    data_by_startyear_by_allocation = {}
+    for allocation in data_by_allocations.keys():
+        data_by_startyear = data_func(data_by_allocations[allocation], *argv)
+        data_by_startyear_by_allocation[allocation] = data_by_startyear
+    return data_by_startyear_by_allocation
--- portfoliofinder/portfolio_value_by_startyear_by_allocation.py
+++ portfoliofinder/portfolio_value_by_startyear_by_allocation.py
@@ -1,30 +1,11 @@
-import pandas as pd
-from functools import reduce
+from .data_by_startyear_by_allocation import DataByStartYearByAllocation
+from .portfolio_value_by_startyear import _get_portfolio_value_by_startyear
 
 from .contributions import Contributions
-from .portfolio_value_by_startyear import _get_portfolio_value_by_startyear
-from ._convert_to_dataframe_by_allocation import _convert_to_dataframe_by_allocation
-from .stats import DEFAULT_STATS, get_statistics_by_allocation
 
-class PortfolioValuesByStartYearByAllocation():
+class PortfolioValuesByStartYearByAllocation(DataByStartYearByAllocation):
 
     def __init__(self, portfolio_returns_by_allocation: dict, timeframe, contributions: Contributions):
-        self._portfolio_value_by_startyear_by_allocation = _get_portfolio_value_by_startyear_by_allocation(
-            portfolio_returns_by_allocation, timeframe, contributions)
-
-    def to_series(self, allocation) -> pd.Series:
-        return self._portfolio_value_by_startyear_by_allocation[allocation]
-
-    def to_dataframe(self) -> pd.DataFrame:
-        return _convert_to_dataframe_by_allocation(self._portfolio_value_by_startyear_by_allocation)
-
-    def get_statistics(self, statistics = DEFAULT_STATS) -> pd.Series:
-        return get_statistics_by_allocation(self._portfolio_value_by_startyear_by_allocation, statistics)
-
-
-def _get_portfolio_value_by_startyear_by_allocation(portfolio_returns_by_allocations, timeframe, contributions):
-    portfolio_value_by_startyear_by_allocation = {}
-    for allocation in portfolio_returns_by_allocations.keys():
-        portfolio_value_by_startyear = _get_portfolio_value_by_startyear(portfolio_returns_by_allocations[allocation], timeframe, contributions)
-        portfolio_value_by_startyear_by_allocation[allocation] = portfolio_value_by_startyear
-    return portfolio_value_by_startyear_by_allocation
+        DataByStartYearByAllocation.__init__(self,
+                                             _get_portfolio_value_by_startyear,
+                                             portfolio_returns_by_allocation, timeframe, contributions)
