22c7d6964d96782048ca496f930ac4d361122697
=Andrew Steffey <asteffey@gmail.com>
2019-04-14 14:54:59 -0400

    Read test file relative to testdata_reader location

--- tests/testdata_reader.py
+++ tests/testdata_reader.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import os
 
-_TEST_DATA_PATH = "tests/test_results.xlsx"
+
+_TEST_DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/test_results.xlsx"
 
 def read_dataframe(sheet_name, usecols=None):
     return pd.read_excel(_TEST_DATA_PATH, sheet_name, index_col=0, usecols=usecols)
